c496dfa6155bed9db4cc9f8aa701044c2daf7c86
syukur.musa <syukur.musa@cloud-connect.asia>
2024-09-18 18:54:32 +0800

    fix port

--- common.py
+++ common.py
@@ -6,6 +6,7 @@ from datetime import datetime, date, timedelta
 import redis
 
 endpoint = os.environ['API_ENDPOINT']
+api_port = os.environ['API_PORT']
 redis_host = os.environ['REDIS_HOST']
 redis_port = os.environ['REDIS_PORT']
 
@@ -14,7 +15,7 @@ redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
 logging.basicConfig(level=logging.DEBUG)
 
 def refresh_token(publicId):
-	response = requests.post(f'{endpoint}/api/generate', json={'publicId':str(publicId)})
+	response = requests.post(f'{endpoint}:{api_port}/api/generate', json={'publicId':str(publicId)})
 	if response.status_code == 200:
 		data = response.json()
 		token = data['token']
